4678b09b7d5ad5cd6bf63afc24d1044b5c9dee56
mojtabababiker <mojmohammad98@gmail.com>
2024-03-31 01:59:40 +0200

    Update models init file, move the prediction model to the api

--- models/__init__.py
+++ models/__init__.py
@@ -3,9 +3,7 @@ Description: This file initializes the storage variable and
 reloads the data from the database.
 """
 from models.engine.db_storage import DatabaseStorage
-from models.ml_models.predictor import Predictor
 
 storage = DatabaseStorage() # create an instance of the DatabaseStorage class
 storage.reload() # reload the data from the database
-predictor = Predictor() # create an instance of the machine learning Predictor class
 TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f' # set the time format

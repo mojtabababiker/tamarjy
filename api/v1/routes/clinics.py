49ff550440bbf61bcfe47c87aff27008bcda6edf
mojtabababiker <mojmohammad98@gmail.com>
2024-03-31 23:48:48 +0200

    Update tamarjy api, add the appointments date feature

--- api/v1/routes/clinics.py
+++ api/v1/routes/clinics.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 """API clinics view module"""
+from datetime import datetime
+from datetime import timedelta
 from flask import jsonify, request
 from api.v1.routes import app_routes
 from models import storage, TIME_FORMAT
@@ -44,7 +46,6 @@ def get_clinics():
 @app_routes.route('clinics/reserve', methods=['POST'])
 def reserve_clinic():
     """Reserve a clinic for the user"""
-    from datetime import datetime  # pylint: disable=import-outside-toplevel
     data = request.get_json()
     if not data:
         return jsonify({"error": "Not a JSON"}), 400
@@ -70,4 +71,68 @@ def reserve_clinic():
     )
     appointment_.save()
     return jsonify({"message": "Appointment reserved successfully"}), 201
-    
+
+@app_routes.route('clinics/dates/<clinic_id>', methods=['GET'])
+def get_clinic_date(clinic_id):
+    """Get all the available dates for the clinic"""
+    day = request.args.get('day')
+    clinic = storage.get('Clinic', filters={'id': clinic_id})
+    if not clinic or not clinic[0]:
+        return jsonify({"error": "Clinic not found"}), 404
+    clinic = clinic[0]
+    # no day provided in the request
+    if not day:
+        date_format = "%Y-%m-%d"
+        dates = []
+        today = datetime.now()
+        # create and return the next 7 days dates
+        for i in range(7):
+            date = today + timedelta(days=i)
+            if i == 0:
+                dates.append("Today")
+            elif i == 1:
+                dates.append("Tomorrow")
+            elif date.strftime(date_format) not in dates:
+                dates.append(date.strftime(date_format))
+        return jsonify({"dates": dates, "status": "success"}), 200
+    # day provided in the request
+    # get the appointments for the clinic
+    appointments = clinic.reservations
+    # get all the appointments for the provided day
+    appointments_time = [appointment.time for appointment in appointments if appointment.day == day]
+    time_format = "%H:%M"
+    times = []
+    if day == "Today":
+        day = datetime.now()
+        # wrap the time to the next hour if current time is passed current hour
+        if day.minute > 0:
+            if day.hour == 23:
+                to_prev_hour = day.minute
+                day -= timedelta(minutes=to_prev_hour)
+            else:
+                to_next_hour = 60 - day.minute
+                day += timedelta(minutes=to_next_hour)
+
+    elif day == "Tomorrow":
+        day = datetime.now()
+        to_next_day = 24 - (day.hour + (day.minute / 60))
+        day += timedelta(hours=to_next_day)
+        print(day)
+
+    else:
+        try:
+            day = datetime.strptime(day ,TIME_FORMAT)
+        except Exception:
+            return jsonify({"error": "Invalid date format"}), 400
+
+    current_day = day.day
+    for i in range(24):
+        date = day + timedelta(hours=i)
+        print(date.day)
+        print(date.hour)
+        if current_day != date.day:
+            break
+        if (date.strftime(time_format) not in appointments_time  and date.hour >= 16):
+            times.append(date.strftime(time_format))
+
+    return jsonify({"times": times, "status": "success"}), 200
